c2f82d21b6429707661ae10f4d8240480074b11e
CharcoalNuggets <dgin@mail.csuchico.edu>
2018-04-11 17:33:33 -0700

    Functions are weird

--- main.cpp
+++ main.cpp
@@ -8,60 +8,66 @@ const int MAX_TEAMS = 4;
 const int MIN_PERIODS = 1;
 const int MIN_TEAMS = 1;
 
+void printScoreboard(vector< vector<int> >);
+
+
 int main()
 {
-  int periods;
-  int teams;
-  int playerCount = 1;
-
-  vector<vector<int> > board;
-
-  cout<<"How many competitors? ";
-  cin>>teams;
-  cout<<"How many scoring periods? ";
-  cin>>periods;
-
-  if( teams < MIN_TEAMS || periods < MIN_PERIODS || 
-      teams > MAX_TEAMS || periods > MAX_PERIODS )
-  {
-    cout<<"Must have between "<<MIN_TEAMS<<" and "<<MAX_TEAMS<<" competitors.\n";
-    cout<<"and between "<<MIN_PERIODS<<" and "<<MAX_PERIODS<<" periods.\n";
-    return 0;
-  }
-  else
-  {
-    board.resize(teams);
-
-    for(int r=0; r<board.size(); r++)
-    {
-      board[r].resize(periods);
-    }
-  }
+        int periods;
+        int teams;
 
 
-  for(int r=0; r<board.size(); r++)
-  {
-    for(int c=0; c<board[r].size(); c++)
-    {
-      board[r][c] = 0;
-    }
-  }
+        vector< vector<int> > board;
 
-  cout<<"SCOREBOARD\n";
-  
-  for(int r=0; r<board.size(); r++)
-  { 
-    cout<<"Player "<< playerCount <<": ";
-    for(int c=0; c<board[r].size(); c++)
-    {
-      cout<< board[r][c] <<"|";
-    }
-    cout<<endl;
-    playerCount++;
-  }
-   //make scoreboard and fill it with zeros
-  
-   //once created, display the scoreboard
-  
-  return 0;
+        cout<<"How many competitors? ";
+        cin>>teams;
+        cout<<"How many scoring periods? ";
+        cin>>periods;
+
+        if( teams < MIN_TEAMS || periods < MIN_PERIODS || 
+                        teams > MAX_TEAMS || periods > MAX_PERIODS )
+        {
+                cout<<"Must have between "<<MIN_TEAMS<<" and "<<MAX_TEAMS<<" competitors.\n";
+                cout<<"and between "<<MIN_PERIODS<<" and "<<MAX_PERIODS<<" periods.\n";
+                return 0;
+        }
+        else
+        {
+                board.resize(teams);
+
+                for(int r=0; r<board.size(); r++)
+                {
+                        board[r].resize(periods);
+                }
+        }
+
+
+        for(int r=0; r<board.size(); r++)
+        {
+                for(int c=0; c<board[r].size(); c++)
+                {
+                        board[r][c] = 0;
+                }
+        }
+
+        printScoreboard(board);
+
+        return 0;
 }
+
+void printScoreboard(vector< vector<int> > grid)
+{
+        cout<<"SCOREBOARD\n";
+
+        for(int r=0; r<grid.size(); r++)
+        {
+                
+                cout<<"Player "<< r+1 <<": ";
+                for(int c=0; c<grid[r].size(); c++)
+                {
+                        cout<< grid[r][c] <<"|";
+                }
+                cout<<endl;          
+        }
+}
+
